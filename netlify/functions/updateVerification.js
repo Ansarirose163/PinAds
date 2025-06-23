384e567dbcaf018d040a006fe7aad6b96d6e488a
Ansarirose163 <149314215+Ansarirose163@users.noreply.github.com>
2025-06-23 18:36:33 +0300

    Update updateVerification.js

--- netlify/functions/updateVerification.js
+++ netlify/functions/updateVerification.js
@@ -1,26 +1,28 @@
+// updateVerification.js
 const { addDevice } = require('./verifiedDevices');
 
 exports.handler = async function(event) {
-  try {
-    const { deviceId } = JSON.parse(event.body);
-
-    if (!deviceId) {
-      return {
-        statusCode: 400,
-        body: JSON.stringify({ error: "Device ID required" }),
-      };
-    }
-
-    addDevice(deviceId);
-
+  if (event.httpMethod !== 'POST') {
     return {
-      statusCode: 200,
-      body: JSON.stringify({ success: true }),
-    };
-  } catch (err) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: err.message }),
+      statusCode: 405,
+      body: JSON.stringify({ error: 'Method Not Allowed' }),
     };
   }
+
+  const data = JSON.parse(event.body || '{}');
+  const deviceId = data.deviceId;
+
+  if (!deviceId) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: 'deviceId required' }),
+    };
+  }
+
+  addDevice(deviceId);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ success: true }),
+  };
 };
