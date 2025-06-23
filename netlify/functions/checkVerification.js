aab5fbc8a2f2cd5a71091042eb1e80d62a60884b
Ansarirose163 <149314215+Ansarirose163@users.noreply.github.com>
2025-06-23 17:50:11 +0300

    Update checkVerification.js

--- netlify/functions/checkVerification.js
+++ netlify/functions/checkVerification.js
@@ -1,15 +1,10 @@
+const { isDeviceVerified } = require('./verifiedDevices');
+
 exports.handler = async function(event) {
   const { deviceId } = event.queryStringParameters;
 
-  if (deviceId === "eaf400fd0094fc9f") {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ verified: true })
-    };
-  }
-
   return {
     statusCode: 200,
-    body: JSON.stringify({ verified: false })
+    body: JSON.stringify({ verified: isDeviceVerified(deviceId) })
   };
 };
