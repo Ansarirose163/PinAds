9cd71b24677cebed482f192b295edea799cae331
Ansarirose163 <149314215+Ansarirose163@users.noreply.github.com>
2025-06-23 18:00:51 +0300

    Update verifiedDevices.js

--- netlify/functions/verifiedDevices.js
+++ netlify/functions/verifiedDevices.js
@@ -1,12 +1,14 @@
-// Simple in-memory store for verified devices (reset on deploy/restart)
 const verifiedDevices = new Set();
 
 function addDevice(deviceId) {
   verifiedDevices.add(deviceId);
 }
 
-function isVerified(deviceId) {
+function isDeviceVerified(deviceId) {
   return verifiedDevices.has(deviceId);
 }
 
-module.exports = { addDevice, isVerified };
+module.exports = {
+  addDevice,
+  isDeviceVerified
+};
